92696401aaa936803d30e875a113544745447947
Teng Yu <raffaello_yu@outlook.com>
2024-09-02 09:01:57 +0800

    Delete RemoveElement.py

--- RemoveElement.py
+++
@@ -1,9 +0,0 @@
-nums = [3,2,2,3]
-val = 3
-
-class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        print("before: ", nums)
-        while val in nums:
-            nums.remove(val)
-        print("removed: ", nums)
